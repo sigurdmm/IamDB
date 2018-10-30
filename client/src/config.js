4b951c2396cdf0dd8a754f0750d63351f5f87c67
Fredrik F Lindhagen <fred.lindh96@gmail.com>
2018-10-30 18:15:07 +0100

    Fix files failing on eslint (client)

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,4 @@
 
 
+// eslint-disable-next-line import/prefer-default-export
 export const isDev = () => process.env.NODE_ENV === 'development';
--- client/src/modules/media/api.js
+++ client/src/modules/media/api.js
@@ -1,6 +1,6 @@
-
 /**
  *
  * */
-export const fetchMediaById = (id) => fetch(`/api/v1/media/${id}`, { credentials: 'same-origin' })
+// eslint-disable-next-line import/prefer-default-export
+export const fetchMediaById = id => fetch(`/api/v1/media/${id}`, { credentials: 'same-origin' })
   .then(res => res.json());
