c992873ded6774f0ad517d0b78eb62cf46d7d5e3
Fredrik F Lindhagen <fred.lindh96@gmail.com>
2018-11-01 11:32:39 +0100

    Setup debug handlers for mongoose in dev

--- models/media.js
+++ models/media.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 
 const MediaSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  description: { type: String },
+  description: String,
   rating: { type: Number, default: 0.0 },
   imdbid: { type: String, unique: true, required: true },
   released: { type: Date, default: Date.now() },
   actors: { type: Array, default: [] },
-  director: { type: String },
+  director: String,
   thumbnails: {
     small: String,
     large: String
@@ -31,4 +31,21 @@ MediaSchema.index(
     }
   });
 
-module.exports = mongoose.model('Media', MediaSchema);
+const Media = mongoose.model('Media', MediaSchema);
+
+Media.on('index', (err) => {
+  if (err) {
+    console.error('Failed to create index for Media', err);
+    return;
+  }
+
+  console.debug('Indices for collection Media has been created');
+
+  if (process.env.NODE_ENV === 'development') {
+    Media.collection.getIndexes({ full: true })
+      .then(indices => console.debug('Indices created', indices))
+      .catch(console.error);
+  }
+});
+
+module.exports = Media;
