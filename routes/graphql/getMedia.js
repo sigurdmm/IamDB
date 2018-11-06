e52f2cd4a86b218db51bc5c77c1093b85fcb9244
Fredrik F Lindhagen <fred.lindh96@gmail.com>
2018-11-06 16:22:15 +0100

    Auto populate list of actors

--- routes/graphql/getMedia.js
+++ routes/graphql/getMedia.js
@@ -3,7 +3,7 @@ const Media = require('../../models/media');
 const getMedia = async (query) => {
   const { id } = query;
 
-  return await Media.findById(id);
+  return await Media.findById(id).populate('actors').exec();
 };
 
 module.exports = getMedia;
