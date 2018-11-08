3105b99f35373be6a9a4d6cf957bd9ca295c9146
Ingrid Domben <ingrid.domben@gmail.com>
2018-11-08 19:48:13 +0100

    Add route for filmpage

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,9 +1,11 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import FilmPage from './pages/FilmPage';
 
 const Routes = () => <BrowserRouter>
   <Switch>
     <Route exact path="/" component={HomePage}/>
+    <Route exact path="/media/:id" component={FilmPage}/>
   </Switch>
 </BrowserRouter>;
 
