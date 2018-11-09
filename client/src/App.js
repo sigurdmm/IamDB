b51a75e38db463444cef74772c955f1316fa376d
Ingrid Domben <ingrid.domben@gmail.com>
2018-11-09 13:47:41 +0100

    #12 Move homepage background to homepage

--- client/src/App.js
+++ client/src/App.js
@@ -4,21 +4,7 @@ import './App.less';
 
 import Routes from './Routes';
 
-const coverimages = [
-  'https://media.giphy.com/media/XUTz8zpPF458I/giphy.gif',
-  'https://media.giphy.com/media/hCm2X1kXxjyZq/giphy.gif',
-  'https://media.giphy.com/media/xpBiaXFbVUtTa/giphy.gif',
-  'https://media.giphy.com/media/PrGNf7O36heCs/giphy.gif',
-  'https://media.giphy.com/media/26BRuo6sLetdllPAQ/giphy.gif',
-  'https://media.giphy.com/media/3ohzdUi5U8LBb4GD4s/giphy.gif',
-  'https://media.giphy.com/media/3oEjI1erPMTMBFmNHi/giphy.gif',
-];
-
-const selectRandomItem = list => list[Math.floor(Math.random() * list.length)];
-
-const App = () => <>
-  <div className="systemcover"
-       style={{ backgroundImage: `url('${selectRandomItem(coverimages)}')` }}/>
+const App = () =>
   <div className="application application--light">
     <Header title="Hello world" className="application__header">
       <nav className="navigation">
@@ -29,7 +15,7 @@ const App = () => <>
     <footer className="application__footer">
       Copyright 2018
     </footer>
-  </div>
-</>;
+  </div>;
+
 
 export default App;
