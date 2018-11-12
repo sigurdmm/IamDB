6f44dc505c8f76c036ecbc7e26bda0c698025406
Ingrid Domben <ingrid.domben@gmail.com>
2018-11-12 14:34:34 +0100

    Handle image in child component

--- client/src/components/CoverDisplay/CoverImage.js
+++ client/src/components/CoverDisplay/CoverImage.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import defaultImage from '../../../public/no-image-found.jpg';
 
 const CoverImage = ({ link, id }) => <div className='coverdisplay__gridelement'>
   <Link to={`/media/${id}`}>
-    <img src={link}/>
+    <img src={link ? link.small : defaultImage}/>
   </Link>
 </div>;
 
 CoverImage.propTypes = {
-  link: PropTypes.string.isRequired,
+  link: PropTypes.string,
   id: PropTypes.string,
+};
+
+CoverImage.defaultProps = {
+  link: defaultImage,
 };
 
 export default CoverImage;
--- client/src/components/CoverDisplay/index.js
+++ client/src/components/CoverDisplay/index.js
@@ -3,8 +3,6 @@ import PropTypes from 'prop-types';
 import './index.less';
 import CoverImage from './CoverImage';
 
-const defaultImg = 'https://nti.biz/globalassets/images/cards-286x160px/no-image-found.jpg';
-
 export default class CoverDisplay extends PureComponent {
   static propTypes = {
     media: PropTypes.array,
@@ -17,7 +15,7 @@ export default class CoverDisplay extends PureComponent {
   render() {
     return <div className='coverdisplay'>
       { this.props.media && this.props.media.length > 0
-        ? this.props.media.map((cover, i) => <CoverImage link={cover.thumbnails ? cover.thumbnails.small : defaultImg} id={cover.id} key={`cover-${i}`} />)
+        ? this.props.media.map((cover, i) => <CoverImage link={cover.thumbnails} id={cover.id} key={`cover-${i}`} />)
         : <p className='coverdisplay__error'>NOTHING FOUND</p>}
     </div>;
   }
