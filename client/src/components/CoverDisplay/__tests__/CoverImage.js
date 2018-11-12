b25c8221461e2a70ca19f7969e90601e6d664ed9
Ingrid Domben <ingrid.domben@gmail.com>
2018-11-12 14:36:45 +0100

    Change tests to accommodate changes in components

--- client/src/components/CoverDisplay/__tests__/CoverImage.js
+++ client/src/components/CoverDisplay/__tests__/CoverImage.js
@@ -4,7 +4,7 @@ import CoverImage from '../CoverImage';
 
 describe('<CoverImage/>', () => {
   it('should render img, when link exists', () => {
-    const wrapper = render(<CoverImage link="https://example.com/test.jpg"/>);
+    const wrapper = render(<CoverImage link="https://example.com/test.jpg" id='1'/>);
 
     expect(wrapper).toMatchSnapshot();
   });
--- client/src/components/CoverDisplay/__tests__/index.js
+++ client/src/components/CoverDisplay/__tests__/index.js
@@ -4,16 +4,19 @@ import CoverDisplay from '../index';
 
 const mockedMedia = [
   {
+    id: '1',
     thumbnails: {
       small: 'https://example.com/test.jpg',
     },
   },
   {
+    id: '2',
     thumbnails: {
       small: 'https://example.com/test2.png',
     },
   },
   {
+    id: '3',
     thumbnails: {},
   },
 ];
