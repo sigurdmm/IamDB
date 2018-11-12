43c98778057bc77fe3cf6ff391b239f6bfeb9973
Fredrik F Lindhagen <fred.lindh96@gmail.com>
2018-11-12 11:36:51 +0100

    Refactor background cover to own component

--- client/src/App.js
+++ client/src/App.js
@@ -4,21 +4,10 @@ import './App.less';
 
 import Routes from './Routes';
 
-const coverimages = [
-  'https://media.giphy.com/media/XUTz8zpPF458I/giphy.gif',
-  'https://media.giphy.com/media/hCm2X1kXxjyZq/giphy.gif',
-  'https://media.giphy.com/media/xpBiaXFbVUtTa/giphy.gif',
-  'https://media.giphy.com/media/PrGNf7O36heCs/giphy.gif',
-  'https://media.giphy.com/media/26BRuo6sLetdllPAQ/giphy.gif',
-  'https://media.giphy.com/media/3ohzdUi5U8LBb4GD4s/giphy.gif',
-  'https://media.giphy.com/media/3oEjI1erPMTMBFmNHi/giphy.gif',
-];
-
-const selectRandomItem = list => list[Math.floor(Math.random() * list.length)];
+import ApplicationAnimationCover from './components/ApplicationAnimationCover';
 
 const App = () => <>
-  <div className="systemcover"
-       style={{ backgroundImage: `url('${selectRandomItem(coverimages)}')` }}/>
+  <ApplicationAnimationCover/>
   <div className="application application--light">
     <Header title="Hello world" className="application__header">
       <nav className="navigation">
