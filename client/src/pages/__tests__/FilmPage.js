5303cd0c810e77922cb778aaab58e846257bf578
Ingrid Domben <ingrid.domben@gmail.com>
2018-11-13 11:22:24 +0100

    Add test for film page

---
+++ client/src/pages/__tests__/FilmPage.js
@@ -0,0 +1,27 @@
+import { render } from 'enzyme';
+
+// Note we're not using the default exported version
+import { FilmPage } from '../FilmPage';
+
+const mockedDetails = {
+  id: -1,
+  name: '',
+  description: '',
+  director: '',
+  type: 'movie',
+  thumbnails: {},
+};
+
+describe('<FilmPage/>', () => {
+  it('should render properly', () => {
+    const spy = jest.fn();
+    const wrapper = render(<FilmPage
+      fetchMediaById={spy}
+      detailedMedia={mockedDetails}
+      loading={false}
+      error={null}
+      />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+});
