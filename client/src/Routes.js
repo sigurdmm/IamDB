33258071d1a8b7f8c3dd6f98bd73912e6949d33b
Ingrid Domben <ingrid.domben@gmail.com>
2018-11-15 15:33:45 +0100

    Add route for actor page

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -6,10 +6,14 @@ import HomePage from './pages/HomePage';
 // eslint-disable-next-line import/no-named-as-default
 import FilmPage from './pages/FilmPage';
 
+// eslint-disable-next-line import/no-named-as-default
+import ActorPage from './pages/ActorPage';
+
 const Routes = () => <BrowserRouter>
   <Switch>
     <Route exact path="/" component={HomePage}/>
     <Route exact path="/media/:id" component={FilmPage}/>
+    <Route exact path="/actor/:id" component={ActorPage}/>
   </Switch>
 </BrowserRouter>;
 
