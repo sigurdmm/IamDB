5791f516ee60aba94a4f8dcf480f720532c43bd4
Fredrik F Lindhagen <fred.lindh96@gmail.com>
2018-11-15 09:31:59 +0100

    Dynamically change header style on route change

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
 import Header from './components/Header';
 import './App.less';
 
 import Routes from './Routes';
 
-const App = () => <div className="application application--light">
-  <Header title="Hello world" className="application__header">
-    <nav className="navigation">
-      <a href="#" className="navigation__link">Test</a>
-    </nav>
-  </Header>
-  <Routes/>
-  <footer className="application__footer">
-    Copyright 2018
-  </footer>
-</div>;
+const App = ({ location }) => {
+  // Used to determine what styles to apply to the application
+  const isOnRoot = location.pathname === '/';
 
+  return <div className="application application--light">
+    <Header title="Media searcher"
+            className={`application__header ${!isOnRoot ? 'application__header--filled' : ''}`}/>
+    <Routes/>
+    <footer className={`application__footer ${!isOnRoot ? 'application__footer--dark' : ''}`}>
+      Copyright 2018
+    </footer>
+  </div>;
+};
 
-export default App;
+App.propTypes = {
+  location: PropTypes.object.isRequired,
+};
+
+export default withRouter(App);
