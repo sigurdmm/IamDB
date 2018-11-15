e1380ee0d25221e4766f4cf83834ffdc7974b7b5
Sigurd Marius Melsom <sigurdmm@stud.ntnu.no>
2018-11-15 23:03:07 +0100

    Closes #2. Write tests for Sorting

---
+++ client/src/components/Sorting/__tests__/index.js
@@ -0,0 +1,33 @@
+import { shallow } from 'enzyme';
+
+import Sorting from '../index';
+
+
+describe('<Sorting/>', () => {
+  it('should render properly', () => {
+    const wrapper = shallow(<Sorting
+      directionValue={1}
+      onSort={jest.fn()}
+      onDirectionClick={jest.fn()}
+      sortingMethods={ [{ label: 'label', value: 'value' }, { label: 'label2', value: 'value2' }] }
+      visible={true}
+    />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should call onDirectionClick on click', () => {
+    const spy = jest.fn();
+
+    const wrapper = shallow(<Sorting
+        directionValue={1}
+        onSort={jest.fn()}
+        onDirectionClick={spy}
+        sortingMethods={ [{ label: 'label', value: 'value' }] }
+        visible={true}
+      />);
+
+    wrapper.find('button').simulate('click');
+    expect(spy).toHaveBeenCalled();
+  });
+});
