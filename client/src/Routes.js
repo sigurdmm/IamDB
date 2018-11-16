724cde6c1acbeda66a634f23f270482deec2c158
Fredrik F Lindhagen <fred.lindh96@gmail.com>
2018-11-16 11:27:12 +0100

    Add default route which redirects to home

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,4 +1,4 @@
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 // eslint-disable-next-line import/no-named-as-default
 import HomePage from './pages/HomePage';
@@ -9,6 +9,7 @@ import FilmPage from './pages/FilmPage';
 const Routes = () => <Switch>
   <Route exact path="/" component={HomePage}/>
   <Route exact path="/media/:id" component={FilmPage}/>
+  <Redirect exact to="/"/>
 </Switch>;
 
 
