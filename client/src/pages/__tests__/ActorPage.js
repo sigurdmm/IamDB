ff65ac3383eea8b2588e7ff4e0f397b20691ce7d
Ingrid Domben <ingrid.domben@gmail.com>
2018-11-16 15:12:52 +0100

    Improve test coverage

--- client/src/pages/__tests__/ActorPage.js
+++ client/src/pages/__tests__/ActorPage.js
@@ -1,3 +1,4 @@
+import { MemoryRouter } from 'react-router-dom';
 import { mount, render } from 'enzyme';
 
 // Note we're not using the default exported version
@@ -7,46 +8,55 @@ const mockedDetails = {
   id: -1,
   name: '',
   thumbnails: {},
-  media: [],
+  media: [{
+    name: 'movie',
+    director: 'director',
+  }],
   popularity: 0,
 };
 
 describe('<ActorPage/>', () => {
   it('should render properly', () => {
+    const mockedDetailsNoDirector = Object.assign(mockedDetails, {});
+    // Remove directors
+    mockedDetailsNoDirector.media.map(m => Object.assign(m, { director: null }));
+
     const spy = jest.fn();
-    const wrapper = render(<ActorPage
-      fetchActorById={spy}
-      detailedActor={mockedDetails}
-      loading={false}
-      error={null}
-    />);
+    const wrapper = render(<MemoryRouter>
+      <ActorPage
+        fetchActorById={spy}
+        detailedActor={mockedDetailsNoDirector}
+        loading={false}
+        error={null}
+      />
+    </MemoryRouter>);
 
     expect(wrapper).toMatchSnapshot();
   });
   it('should render properly when loading', () => {
     const spy = jest.fn();
-    const wrapper = render(<ActorPage
+    const wrapper = render(<MemoryRouter><ActorPage
       fetchActorById={spy}
       detailedActor={mockedDetails}
       loading={true}
       error={null}
-    />);
+    /></MemoryRouter>);
     expect(wrapper).toMatchSnapshot();
   });
   it('should render properly when error', () => {
     const spy = jest.fn();
-    const wrapper = render(<ActorPage
+    const wrapper = render(<MemoryRouter><ActorPage
       fetchActorById={spy}
       detailedMedia={mockedDetails}
       loading={false}
       error={true}
-    />);
+    /></MemoryRouter>);
     expect(wrapper).toMatchSnapshot();
   });
   it('should fetch detailedActor', () => {
     const id = 1337;
     const spy = jest.fn();
-    mount(<ActorPage
+    mount(<MemoryRouter><ActorPage
       fetchActorById={spy}
       match={{
         params: { id },
@@ -54,7 +64,7 @@ describe('<ActorPage/>', () => {
       detailedActor={mockedDetails}
       loading={false}
       error={null}
-    />);
+    /></MemoryRouter>);
     expect(spy).toHaveBeenCalled();
     expect(spy).toHaveBeenCalledWith(id);
   });
